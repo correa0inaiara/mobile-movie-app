d8e70e99d9c0c1decb9d285bf260151d135dd6be
Naiara Correa <correa0inaiara@gmail.com>
2025-12-22 18:00:39 -0300

    searchbar and fetchmovie call

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,31 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
-// import "./globals.css"
+import { ScrollView, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
+import { useRouter } from 'expo-router'
 
 export default function Index() {
+  const router = useRouter()
+
   return (
-    <View className="flex-1 justify-center items-center">
-      {/* <Text className="text-5xl text-[#123123] font-bold">Teste</Text>
-      <Text className="text-5xl text-accent font-bold">Teste</Text>
-      <Text className="text-5xl text-dark-200 font-bold">Teste</Text> */}
-      <Text className="text-5xl text-primary font-bold">Welcome!</Text>
-      {/* <Link href="/onboarding">Onboarding</Link>
-      <Link href="/movie/avengers">Avengers Movie</Link> */}
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '110%', paddingBottom: 10}}>
+        <Image 
+          source={icons.logo} 
+          style={{ width: 48, height: 40, marginTop: 80, marginBottom: 20, alignSelf: 'center' }} 
+        />
+
+        <View className="mt-5">
+          <SearchBar
+            onPress={() => router.push('/search')}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
